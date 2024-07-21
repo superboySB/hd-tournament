814e5975f32b36b8f8c49582e9a85dcacff25a91
superboySB <604896160@qq.com>
2024-07-21 17:17:11 +0800

    开始写占热区

--- demo_requ_0721.py
+++ demo_requ_0721.py
@@ -3,23 +3,32 @@ import numpy as np
 from hddf2sim.hddf2sim import HDDF2Sim
 from hddf2sim.conf import default_conf
 
-# from agents.team_blue.blue_agent_new import Agent as BlueAgent
 from agents.team_blue.blue_agent_demo import Agent as BlueAgent
-from agents.houlang0715.agent import Agent as RedAgent
 
-with open("scen_0715.json", "r") as fin:
+# from agents.team_blue.blue_agent_demo import Agent as RedAgent
+from agents.houlang.agent import Agent as RedAgent
+
+with open("scen_latest.json", "r") as fin:
     scen = json.load(fin)
 
 sim = HDDF2Sim(scen, use_tacview=True, save_replay=True, replay_path="replay.acmi")
 sim.reset()
 red_agent = RedAgent('red')
 blue_agent = BlueAgent('blue')
+num_step = 0
 
 while not sim.done:
+    num_step += 1 
     cmds = []
     red_obs = sim.get_obs(side='red')
     red_cmd_dict = red_agent.step(red_obs)
-    print(red_cmd_dict)
+
+    # for key, value in red_obs.my_planes.items():
+    #     if num_step % 10 == 0:
+    #         print(f"Step: {num_step}")
+    #         print(f"Plane ID: {key}, My plane coordinates: (x: {value.x}, y: {value.y}, z: {value.z}, yaw: {value.yaw}, v_north: {value.v_north}, v_east: {value.v_east}, v_down: {value.v_down})")
+    #         print(f"Control: aileron={red_cmd_dict[key]['control'][0]}, elevator={red_cmd_dict[key]['control'][1]}, rudder={red_cmd_dict[key]['control'][2]}, throttle={red_cmd_dict[key]['control'][3]}")
+
     sim.send_commands(red_cmd_dict, cmd_side='red')
     blue_obs = sim.get_obs(side='blue')
     blue_cmd_dict = blue_agent.step(blue_obs)
