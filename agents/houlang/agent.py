f4715e15a827b990fdee64ccb402a21d91b58d34
superboySB <604896160@qq.com>
2024-08-02 01:54:35 +0800

    添加发弹逻辑

--- agents/houlang/agent.py
+++ agents/houlang/agent.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 from sturnus.geo import *
-from .funcs import FlyPid, Vector3, degrees_limit, is_facing_missile  # 确保 FlyPid 模块正确引用
+from .funcs import FlyPid, Vector3, degrees_limit, is_facing_target  # 确保 FlyPid 模块正确引用
 
 last_target_pitch = 0
 last_target_heading = 0
@@ -33,7 +33,6 @@ class Agent:
         self.mid_missile_time = {}    # 记录中距弹上一次打弹时间，用于设置打弹cd
         self.short_missile_time = {}    # 记录近距弹上一次打弹时间，用于设置打弹cd
         self.mid_lock_time = 0
-        self.mid_lock_target_ind = 0
     
     def calculate_distance_2d(self, plane_info, missile_info):
         return math.sqrt(
@@ -137,20 +136,7 @@ class Agent:
                 action[2] = 1
                 if debug:
                     print("匀速", end=' ')
-        elif mode == "plane":
-            if target.tas - plane.tas > 10:
-                action[2] = 0
-                if debug:
-                    print("加速", end=' ')
-            elif target.tas - plane.tas < -10:
-                action[2] = 2
-                if debug:
-                    print("减速", end=' ')
-            else:
-                action[2] = 1
-                if debug:
-                    print("匀速", end=' ')
-        elif mode == "missile":
+        elif mode in ["missile","plane"]:
             action[2] = 0
             if debug:
                 print("加速", end=' ')
@@ -161,38 +147,102 @@ class Agent:
 
     def get_weapon_launch_info(self, obs, my_plane):
         weapon_launch_info = {}
+
         if len(my_plane.mid_lock_list)>0 and \
-            my_plane.loadout.get('mid_missile', 0)>0 and obs.sim_time - self.mid_missile_time.get(my_plane.ind, 0) > 10.0:
-                weapon_launch_info = {
-                    'type': 'mid_missile',
-                    'target': my_plane.mid_lock_list[0],
-                }
-                self.mid_missile_time[my_plane.ind] = obs.sim_time
-                self.mid_lock_target_ind = my_plane.mid_lock_list[0]
+            my_plane.loadout.get('mid_missile', 0)>0 and obs.sim_time - self.mid_missile_time.get(my_plane.ind, 0) > 1.0:
+                best_target = None
+                min_distance = float('inf')
+                for target_id in my_plane.mid_lock_list:
+                    if target_id in self.enemy_plane_tracks:
+                        target_positions = np.array(self.enemy_plane_tracks[target_id][-20:])
+                        aircraft_positions = np.array(self.myplane_tracks[my_plane.ind][-20:])
+                        facing_target, _, is_ahead_of_target = is_facing_target(target_positions, aircraft_positions, True)
+                        
+                        if not facing_target and not is_ahead_of_target:
+                            distance = self.calculate_distance_3d(Vector3(*aircraft_positions[-1]), Vector3(*target_positions[-1]))
+                            if distance < min_distance:
+                                min_distance = distance
+                                best_target = target_id
+
+                if best_target:
+                    weapon_launch_info = {
+                        'type': 'mid_missile',
+                        'target': best_target
+                    }
+                    self.mid_missile_time[my_plane.ind] = obs.sim_time
 
         if len(my_plane.short_lock_list)>0 and \
             my_plane.loadout.get('short_missile', 0)>0 and obs.sim_time - self.short_missile_time.get(my_plane.ind, 0) > 5.0:
-                weapon_launch_info = {
-                    'type': 'short_missile',
-                    'target': my_plane.short_lock_list[0],
-                }
-                self.short_missile_time[my_plane.ind] = obs.sim_time
+                best_target = None
+                min_distance = float('inf')
+                for target_id in my_plane.short_lock_list:
+                    if target_id in self.enemy_plane_tracks:
+                        target_positions = np.array(self.enemy_plane_tracks[target_id][-20:])
+                        aircraft_positions = np.array(self.myplane_tracks[my_plane.ind][-20:])
+                        facing_target, _, is_ahead_of_target = is_facing_target(target_positions, aircraft_positions, True)
+                        
+                        if not facing_target and not is_ahead_of_target:
+                            distance = self.calculate_3d_distance(Vector3(*aircraft_positions[-1]), Vector3(*target_positions[-1]))
+                            if distance < min_distance:
+                                min_distance = distance
+                                best_target = target_id
+
+                if best_target:
+                    weapon_launch_info = {
+                        'type': 'short_missile',
+                        'target': best_target
+                    }
+                    self.short_missile_time[my_plane.ind] = obs.sim_time
 
         return weapon_launch_info
+
+    def update_enemy_plane_tracks(self, obs):
+        if len(obs.awacs_infos) and len(self.full_enemy_plane_id_list)==0:
+            for awacs_i in obs.awacs_infos:
+                self.full_enemy_plane_id_list.append(awacs_i.ind)
+                self.enemy_plane_tracks[awacs_i.ind] = []
+        
+        if len(obs.awacs_infos):
+            alive_enemy_inds = [awacs_i.ind for awacs_i in obs.awacs_infos]
+            for enemy_id in self.full_enemy_plane_id_list:
+                if enemy_id not in alive_enemy_inds:
+                    # 如果敌机不再活跃，从跟踪列表中移除该敌机的跟踪信息
+                    if enemy_id in self.enemy_plane_tracks:
+                        del self.enemy_plane_tracks[enemy_id]
+                    # 同时从全敌机列表中移除该敌机
+                    self.full_enemy_plane_id_list.remove(enemy_id)
+
+            percise_visible_enemy_ids = set()
+            for enemy_id, enemy_plane in obs.enemy_planes.items():
+                self.enemy_plane_tracks[enemy_id].append([enemy_plane.x, enemy_plane.y, enemy_plane.z])
+                percise_visible_enemy_ids.add(enemy_id)
+
+            rws_visible_enemy_ids = set()
+            for entity_info in obs.rws_infos:
+                if entity_info.ind in self.full_enemy_plane_id_list and entity_info.ind not in percise_visible_enemy_ids:
+                    self.enemy_plane_tracks[entity_info.ind].append([entity_info.x, entity_info.y, entity_info.z])
+                    rws_visible_enemy_ids.add(entity_info.ind)
+
+            for entity_info in obs.awacs_infos:
+                if entity_info.ind in self.full_enemy_plane_id_list and entity_info.ind not in percise_visible_enemy_ids and \
+                    entity_info.ind not in rws_visible_enemy_ids:
+                    self.enemy_plane_tracks[entity_info.ind].append([entity_info.x, entity_info.y, entity_info.z])
+                
+            max_length = max((len(tracks) for tracks in self.enemy_plane_tracks.values()), default=0)
+            # print("max_length: ", max_length)
+            for enemy_id, tracks in self.enemy_plane_tracks.items():
+                if len(tracks) < max_length:
+                    raise ValueError("各存活敌机的位置记录不可能时间戳对不齐")
+                
 
     def step(self, obs):        
         debug_flag = False
         if self.run_counts % 10 == 0 :
             print("\n------------------------------------------------------------------\n")
             debug_flag = True
-        if len(obs.awacs_infos) and len(self.full_enemy_plane_id_list)==0:
-            for awacs_i in obs.awacs_infos:
-                self.full_enemy_plane_id_list.append(awacs_i.ind)
-                self.enemy_plane_tracks[awacs_i.ind] = []
-        
-        if len(obs.enemy_planes):  # TODO: 在这里添加敌机的跟踪逻辑
-            pass
 
+        self.update_enemy_plane_tracks(obs)
+        
         cmd_dict = {}
         for my_id, my_plane in obs.my_planes.items():
             if not self.ini_pid or my_id not in self.id_pidctl_dict:
@@ -207,8 +257,8 @@ class Agent:
             if self.calculate_distance_2d(my_plane,self.heat_zone_center) >= 15000:
                 self.phase = 1  # 一开始是冲向热区
             else:
-                self.phase = 2 # TODO: 然后开启狗斗模式
-
+                self.phase = 2 # 然后开启狗斗模式
+                
             for entity_info in obs.rws_infos:
                 if entity_info.ind in self.full_enemy_plane_id_list or entity_info.ind in self.expired_missiles:
                     continue
@@ -229,18 +279,18 @@ class Agent:
                             continue
 
                     # 判断威胁
-                    if closest_missile is None or distance < self.calculate_distance_3d(my_plane, closest_missile):
+                    if (closest_missile is None or distance < self.calculate_distance_3d(my_plane, closest_missile)) and (distance < 20000):
                         if len(self.missile_tracks[entity_info.ind])>10:
-                            can_face_missile, _, is_ahead_of_enemy = is_facing_missile(np.array(self.missile_tracks[entity_info.ind][-10:]), 
-                                                        np.array(self.myplane_tracks[my_plane.ind][-10:]))
-                            if can_face_missile and is_ahead_of_enemy: # TODO： 只躲能躲的弹，否则进攻就是最好的防守
+                            can_face_missile, _, is_ahead_of_enemy = is_facing_target(np.array(self.missile_tracks[entity_info.ind][-20:]), 
+                                                        np.array(self.myplane_tracks[my_plane.ind][-20:]))
+                            if can_face_missile and is_ahead_of_enemy: # TODO: 只躲容易躲的弹，否则进攻就是最好的防守
                                 self.phase = 3
                                 self.dangerous_missiles.add(entity_info.ind)
                                 closest_missile = entity_info
 
             if closest_missile:
-                _, can_face_target_position, _ = is_facing_missile(np.array(self.missile_tracks[closest_missile.ind][-10:]), 
-                                                        np.array(self.myplane_tracks[my_plane.ind][-10:]), debug = debug_flag)                                               
+                _, can_face_target_position, _ = is_facing_target(np.array(self.missile_tracks[closest_missile.ind][-20:]), 
+                                                        np.array(self.myplane_tracks[my_plane.ind][-20:]), debug = debug_flag)                                               
                 target_pos = Vector3(can_face_target_position[0],can_face_target_position[1],can_face_target_position[2])
                 action = self.get_action_cmd(target_pos, my_plane, "missile", debug = debug_flag)
                 cmd = {'control': fly_with_alt_yaw_vel(my_plane, action, self.id_pidctl_dict[my_id])}
@@ -251,14 +301,14 @@ class Agent:
                 cmd = {'control': fly_with_alt_yaw_vel(my_plane, action, self.id_pidctl_dict[my_id])}
 
             if self.phase == 2:
-                target_pos = self.heat_zone_center
-                action = self.get_action_cmd(target_pos, my_plane, "fix_point", debug = debug_flag)
+                target_pos = self.heat_zone_center   # TODO: 在热区内开始追击敌人，进攻就是最好的防守
+                action = self.get_action_cmd(target_pos, my_plane, "plane", debug = debug_flag)
                 cmd = {'control': fly_with_alt_yaw_vel(my_plane, action, self.id_pidctl_dict[my_id])}
                 
             # 以上是机动部分
             # ---------------------------------------------------------------------------------------------------
             # 以下是发弹部分
-            #  TODO: 关于发弹的优化还没做，这个可以单独做，不要硬绑在机动的状态里面，不然容易克制不住奇兵
+            # 关于发弹的优化不要硬绑在机动的状态里面，不然容易克制不住奇兵
             weapon_launch_info = self.get_weapon_launch_info(obs,my_plane)
             if len(weapon_launch_info):
                 cmd['weapon'] = weapon_launch_info
